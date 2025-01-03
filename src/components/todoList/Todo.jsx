23dc73a08b8a8ad114899b0ccafdac492c245c8b
Ying Quan <quanying26@hotmail.com>
2025-01-02 22:12:08 -0500

    add delete buttun

--- src/components/todoList/Todo.jsx
+++ src/components/todoList/Todo.jsx
@@ -1,8 +1,23 @@
+import { useDispatch, useSelector } from "react-redux";
+import { deleteTodo } from "../../redux/reducer/todosReducer";
+
 export default function Todo({ todo }) {
+  const  dispatch = useDispatch();
+
   console.log("todo", todo);
+
+  const handleDelete = (id) => {
+    
+    console.log(id)
+      dispatch(deleteTodo(id));
+   
+    
+  };
+
   return (
     <div>
       <span>{todo.task}</span>
+      <button onClick={() => handleDelete(todo.id)}>Delete</button>
     </div>
   );
 }
--- src/redux/reducer/todosReducer.js
+++ src/redux/reducer/todosReducer.js
@@ -25,6 +25,10 @@ export default function todosReducer(state = initialState, action) {
   switch (action.type) {
     case "TODO_ADDED":
       return [...state, action.payload];
+    case "DELETE_TODO":
+      return [...state,
+        
+      ]
 
     default:
       return state;
@@ -34,3 +38,9 @@ export default function todosReducer(state = initialState, action) {
 export const addTodo = (payload) => {
   return { type: "TODO_ADDED", payload };
 };
+
+export const deleteTodo =(id)=>{
+  return{ type: "DELETE_TODO",
+    payload: id
+  }
+}
